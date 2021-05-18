e0d19191e582480895fd9bdb95f6634f81a14336
abdelhady99ly <catcat123496@gmail.com>
2021-05-18 04:45:55 +0300

    table control

--- js/bookingFormControl.js
+++ js/bookingFormControl.js
@@ -1,20 +1,3 @@
-//this is the form's object
-let info = {
-    pname: "lia",
-    gender: "male",
-    age: 12,
-    doctor: "farah",
-    problem: "illness",
-    appDate: 01/05/1888,
-    setter(pname, gender, age, doctor, problem, appDate) {
-        this.pname = pname;
-        this.gender = gender;
-        this.age = age;
-        this.doctor = doctor;
-        this.problem = problem;
-        this.appDate = appDate;
-    }
-}
 
 let submit = document.querySelector('#submit')
 
@@ -31,16 +14,7 @@ info.setter(
     document.querySelectorAll('#appDate').value
  )
  
- 
 
-//this is a test where we change the categories labels to the appointment's information 
- var tester = document.querySelectorAll('.catagories li h3'); 
- tester[0].innerHTML = info.pname;
- tester[1].innerHTML = info.gender;
- tester[2].innerHTML = info.age;
- tester[3].innerHTML = info.doctor;
- tester[4].innerHTML = info.appDate;  
-  
 
 // clear form after submit
 
